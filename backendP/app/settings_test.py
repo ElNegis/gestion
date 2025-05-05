eaf9a6fa9e0c199896b489c331e1524478ccf76c
ElNegis <128728636+ElNegis@users.noreply.github.com>
2025-05-05 17:05:07 -0400

    a
    a

---
+++ backendP/app/settings_test.py
@@ -0,0 +1,19 @@
+# backendP/app/settings_test.py
+from .settings import *
+
+# Usar SQLite en memoria para los tests
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+# Desactivar migraciones para acelerar
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+    def __getitem__(self, item):
+        return None
+
+MIGRATION_MODULES = DisableMigrations()
