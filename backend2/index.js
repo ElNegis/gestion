fe89dc582d4b9c4a7d53a0ea228a50e5757cbb1f
ElNegis <128728636+ElNegis@users.noreply.github.com>
2025-05-07 19:19:05 -0400

    fix

--- backend2/index.js
+++ backend2/index.js
@@ -12,19 +12,6 @@ app.get('/api/query', (req, res) => {
 });
 
 // Listar todas las planchas
-app.get('/planchas', async (req, res) => {
-
-  let data = [];
-  
-  if (admin.apps.length > 0) {
-    const db = admin.firestore();
-    const snapshot = await db.collection('planchas').get();
-    data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-  }
-
-  res.json(data);
-  
-});
 
 export default app;
 
